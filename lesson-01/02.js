0b1d277c07abe9b40124a47bf9202d6381611836
dariushkin <darya95kom@mail.ru>
2023-11-08 11:22:02 +0300

    Task 2, try 2

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -3,7 +3,7 @@
 */
 
 if (number1 % 2 === 0) {
-    console.log(`«${number1} :Это четное число»`)
+    console.log(`${number1} :Это четное число`)
 } else {
-    console.log(`«${number1} :Это нечетное число»`)
+    console.log(`${number1} :Это нечетное число`)
 }
