2e01d104f86a4466adcd2b76ca6ac5a065696944
dariushkin <darya95kom@mail.ru>
2023-11-08 12:15:09 +0300

    Task 4

---
+++ lesson-01/04.js
@@ -0,0 +1,13 @@
+/* Посчитай и выведи окончательный результат в console.log.
+
+let x = 5;
+x += 3;
+x *= 2;
+x -= 4;
+x /= 3;
+*/
+
+console.log(8);
+console.log(10);
+console.log(1);
+console.log(5/3);
