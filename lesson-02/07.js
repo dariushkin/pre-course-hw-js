71508e575bd139b4daeadea7a758d4aa0b583fba
dariushkin <darya95kom@mail.ru>
2024-01-09 16:23:26 +0300

    task 7, try 1

---
+++ lesson-02/07.js
@@ -0,0 +1,14 @@
+let passportMarried = {
+    name: "Petr",
+    surname: "Petrov",
+    address: {
+        country: "USA",
+        city: "Bobryisk"
+    }
+};
+
+let passportMarriedClone = structuredClone(passportMarried);
+passportMarriedClone.married = true;
+
+console.log(passportMarried);
+console.log(passportMarriedClone);
