e01dbebd964672f83d3fca5180aa25993caefebb
dariushkin <darya95kom@mail.ru>
2024-01-20 07:37:53 +0300

    task 1, try 1

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,1 +1,10 @@
-// code
+const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
+
+
+function createColorString() {
+    let trueColors = colors.filter((color) => color === "черный" || color === "красный" ||color === "желтый");
+    return trueColors.join("-");
+
+}
+
+console.log(createColorString());
