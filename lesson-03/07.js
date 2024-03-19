a929dca7342a568704d0c30b48af899f80339fcb
dariushkin <darya95kom@mail.ru>
2024-03-19 13:18:36 +0300

    task 7 try 1

---
+++ lesson-03/07.js
@@ -0,0 +1,15 @@
+const numbers = [
+    [1, 2, 3, 4, 5],
+    [11, 20, 33, 40, 55],
+    [111, 200, 333, 400, 555],
+];
+  
+
+const newNumbers = numbers.filter
+
+
+for (let i = 0 ; i < numbers.length; i++ ) {
+    numbers[i] = numbers[i].filter((arrayNumber) => {
+        return arrayNumber % 2 === 0
+    } )
+}
