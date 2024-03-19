0fc222ba793c8a67daf0155cb13f450cf50ba5bc
dariushkin <darya95kom@mail.ru>
2024-03-19 15:56:54 +0300

    task 7 try 6

--- lesson-03/07.js
+++ lesson-03/07.js
@@ -4,11 +4,10 @@ const numbers = [
     [111, 200, 333, 400, 555],
 ];
   
-
-
 for (let i = 0 ; i < numbers.length; i++ ) {
     numbers[i] = numbers[i].filter((arrayNumber) => {
-        return (arrayNumber % 2 !== 0)
+        return (arrayNumber % 2 === 0)
     } )
-}
-console.log(numbers)
+};
+
+console.log(numbers);
